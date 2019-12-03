5fda2082ea2b154ff2ab772c78ab79d8bf4c9c8c
jstrombe19 <jaredstromberg@gmail.com>
2019-12-02 17:02:22 -0700

    Added validations for User password, specifying a length between 7 and 20 characters; email, uniqueness.

--- weather_or_not_api/app/controllers/users_controller.rb
+++ weather_or_not_api/app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  before_action :get_all_users, only: [:index, :create, :destroy]
+  before_action :find_user_by_id, only: [:destroy]
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: 'Danger, Will Robinson!'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def get_all_users
+    @users = User.all 
+  end
+
+  def find_user_by_id
+    @user = User.find(params[:id])
+  end 
+
 end
--- weather_or_not_api/app/models/user.rb
+++ weather_or_not_api/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :routes
+  validates :password, length: {
+    min: 7,
+    max: 20
+  }
+  validates :email, uniqueness: true
 end
